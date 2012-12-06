76878237e93649645f4091c4ceff14a71aecf962
Kieren Eaton <circledev@gmail.com>
2012-12-06 15:14:13 +0800

    Fixed a crash on the view permissions page

--- role/views/permission/view.php
+++ role/views/permission/view.php
@@ -1,11 +1,19 @@
-	<?
+<?
+$this->title = Yum::t('Permissions List');
+$this->breadcrumbs=array(
+		Yum::t('Permissions')=>array('index'),
+		Yum::t('View'),
+		);
 if(isset($actions))
-	foreach($actions as $action) {
+{
+	foreach($actions as $action)
+	{
 		printf('<h2>%s</h2>', $action->title);
-		echo Yum::t('The following users have permission to perform the action {action}:', array(
-					'{action}' => $action->title));
-		if($action->permissions) {
-			foreach($action->permissions as $permission) {
+		if($action->permissions)
+		{
+			echo Yum::t('The following users have permission to perform this action');
+			foreach($action->permissions as $permission)
+			{
 				echo '<ul>';
 				if($permission->type == 'user') {
 					$user = YumUser::model()->findByPk($permission->principal_id);
@@ -13,12 +21,13 @@ if(isset($actions))
 				}
 				if($permission->type == 'role') {
 					$role = YumRole::model()->findByPk($permission->principal_id);
-					printf('<li>role %s</li>', CHtml::link($role->title, array('/role/view', 'id' => $user->id)));
+					printf('<li>role %s</li>', CHtml::link($role->title, array('/role/view', 'id' => $role->id)));
 				}
 				echo '</ul>';
 			}
 		}
-	} else 
-echo Yum::t('No user has permission');
-?>
+		else
+			echo Yum::t('No users with this action');
+	}
+}
 
