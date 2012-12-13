4175264483498db3a9b6d90054a41a07cf12eb32
Dadeniss <github.com@marco-ilgner.de>
2012-12-13 16:44:21 +0100

    Update friendship/models/YumFriendship.php
    Fixed another path

--- friendship/models/YumFriendship.php
+++ friendship/models/YumFriendship.php
@@ -155,10 +155,10 @@ class YumFriendship extends YumActiveRecord {
 		// If the user has activated email receiving, send a email
 		if($this->isNewRecord)
 			if($user = YumUser::model()->findByPk($this->friend_id))  {
-				if(Yum::hasModule('messages')
+				if(Yum::hasModule('message')
 						&& $user->privacy 
 						&& $user->privacy->message_new_friendship) {
-					Yii::import('application.modules.messages.models.YumMessage');
+					Yii::import('application.modules.message.models.YumMessage');
 					YumMessage::write($user, $this->inviter,
 							Yum::t('New friendship request from {username}', array(
 									'{username}' => $this->inviter->username)),
