38da6a018e05a7b3612ed21589230e7dfc180c8a
dainiuss <dainiusgri@gmail.com>
2013-02-08 17:45:05 -0800

    Update profile/controllers/YumProfileController.php
    Should be profile_id not user_id here. Based on my previous updates.

--- profile/controllers/YumProfileController.php
+++ profile/controllers/YumProfileController.php
@@ -44,7 +44,7 @@ class YumProfileController extends YumController {
 			if(!$user->hasErrors() && !$profile->hasErrors()) {
 				if($user->save() && $profile->save()) {
 					Yum::setFlash('Your changes have been saved');
-					$this->redirect(array('//profile/profile/view', 'id'=>$user->id));
+					$this->redirect(array('//profile/profile/view', 'id'=>$profile->id));
 				}
 			}
 		}
