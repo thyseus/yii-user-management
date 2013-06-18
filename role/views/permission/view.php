d34449358ac6ccca639fe311c101cadf6feeb217
Herbert Maschke <thyseus@gmail.com>
2013-06-18 11:36:17 +0200

    fixed issue 116 index.php?r=role/permission/index not displaying properly

--- role/views/permission/view.php
+++ role/views/permission/view.php
@@ -2,18 +2,22 @@
 if(isset($actions))
 	foreach($actions as $action) {
 		printf('<h2>%s</h2>', $action->title);
-		echo Yum::t('The following users have permission to perform the action {action}:', array(
+		echo Yum::t(
+				'The following users have permission to perform the action {action}:',
+				array(
 					'{action}' => $action->title));
 		if($action->permissions) {
 			foreach($action->permissions as $permission) {
 				echo '<ul>';
 				if($permission->type == 'user') {
 					$user = YumUser::model()->findByPk($permission->principal_id);
-					printf('<li>%s</li>', CHtml::link($user->username, array('/user/view', 'id' => $user->id)));
+					printf('<li>%s</li>', CHtml::link(
+								$user->username, array('/user/view', 'id' => $user->id)));
 				}
 				if($permission->type == 'role') {
 					$role = YumRole::model()->findByPk($permission->principal_id);
-					printf('<li>role %s</li>', CHtml::link($role->title, array('/role/view', 'id' => $user->id)));
+					printf('<li>role %s</li>', CHtml::link(
+								$role->title, array('/role/view', 'id' => $role->id)));
 				}
 				echo '</ul>';
 			}
