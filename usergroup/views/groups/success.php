bb2cf09e9b5910de05ff07398d02bdd13b51d6ee
BeeR <g.s.xbeer@mail.ru>
2014-01-13 19:23:03 +0200

    Translate method change from Yii to Yum

--- usergroup/views/groups/success.php
+++ usergroup/views/groups/success.php
@@ -1,3 +1,7 @@
-<p> The Usergroup <?php echo $model; ?> has been successfully created </p>
+<?php 
+echo Yum::t('The Usergroup {groupname} has been successfully created', array('groupname' => $model))
+?>
 
-<?php echo CHtml::Button(Yii::t('app', 'Back'), array('id' => $relation.'_done')); ?><?php echo CHtml::Button(Yii::t('app', 'Add another Usergroup'), array('id' => $relation.'_create'));
+<?php 
+echo CHtml::Button(Yum::t('Back'), array('id' => $relation.'_done')); 
+echo CHtml::Button(Yum::t('Add another Usergroup'), array('id' => $relation.'_create'));
